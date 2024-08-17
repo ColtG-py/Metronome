5261ea5e152e2d3176fcb6b17d29a40ae1392dcd
Colt Gainey <Colt.Gainey@gmail.com>
2024-08-17 16:31:56 -0400

    readjust sizing

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,10 +4,10 @@ export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full py-4 flex items-center justify-between text-white px-8">
       <a href="https://github.com/ColtG-py" target="_blank" rel="noopener noreferrer">
-        <FaGithub size={24} />  {/* Adjusted size to 24px */}
+        <FaGithub size={36} />  {/* Adjusted size to 24px */}
       </a>
       <a href="https://medium.com/@coltg" target="_blank" rel="noopener noreferrer">
-        <FaMedium size={24} />  {/* Adjusted size to 24px */}
+        <FaMedium size={36} />  {/* Adjusted size to 24px */}
       </a>
     </footer>
   );
