e659760c654ca1ab7ddeecc76c493329c0f39d71
Colt Gainey <Colt.Gainey@gmail.com>
2024-08-17 16:29:11 -0400

    readjust sizing

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,11 +3,11 @@ import { FaGithub, FaMedium } from 'react-icons/fa';
 export default function Footer() {
   return (
     <footer className="fixed bottom-0 w-full py-4 flex items-center justify-between text-white px-8">
-      <a href="https://github.com/your-github-username" target="_blank" rel="noopener noreferrer">
-        <FaGithub size={16} />  {/* Adjusted size to 24px */}
+      <a href="https://github.com/ColtG-py" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={24} />  {/* Adjusted size to 24px */}
       </a>
-      <a href="https://medium.com/@your-medium-username" target="_blank" rel="noopener noreferrer">
-        <FaMedium size={16} />  {/* Adjusted size to 24px */}
+      <a href="https://medium.com/@coltg" target="_blank" rel="noopener noreferrer">
+        <FaMedium size={24} />  {/* Adjusted size to 24px */}
       </a>
     </footer>
   );
